15be4165f948392744742c0c345524b73ff75907
TanmayVidhate <tanmayvidhate7@gmail.com>
2025-01-27 15:57:02 +0530

    User Model Created

--- server/model/User.js
+++ server/model/User.js
@@ -1,5 +1,29 @@
-import { model,Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
-const Userstru = new Schema({
-    
+const stru = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    phone: {
+        type: String,
+        unique: true
+    },
+    address: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+},{
+    timestamps:true
 })
+
+const  User = model("User",stru);
+
+export default User;
